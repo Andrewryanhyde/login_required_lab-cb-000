b672abc4c30a845b660d28bf94f07f098fc00d31
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-04 14:57:06 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
+  def new 
+  end 
 
+  def create 
+    session[:name] = params[:name] if params[:name]
+
+    if !current_user.present? 
+      redirect_to sessions_new_path
+    else 
+      redirect_to '/'
+    end
+  end
 
 end 
